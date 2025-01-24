24353c6666c8c8744a97a0c5a682e6c4aa1e804a
SEOYUNJE <141211605+SEOYUNJE@users.noreply.github.com>
2025-01-24 15:54:40 +0900

    Create predict_format.py

---
+++ utils/predict_format.py
@@ -0,0 +1,6 @@
+def format_pred(labels, boxes, scores):
+    pred_strings = []
+    for label, score, bbox in zip(labels, scores, boxes):
+        xmin, ymin, xmax, ymax = bbox.astype(np.int64)
+        pred_strings.append(f"{label} {score} {xmin} {ymin} {xmax} {ymax}")
+    return " ".join(pred_strings)
