53d8a4a7530f720a49837c85c6b3131416bf830f
SEOYUNJE <141211605+SEOYUNJE@users.noreply.github.com>
2025-01-25 15:47:11 +0900

    Update predict_format.py

--- utils/predict_format.py
+++ utils/predict_format.py
@@ -6,6 +6,6 @@ def format_pred(labels, boxes, scores):
     return " ".join(pred_strings)
 
 ## Submssion File Format
-### ID,TARGET
+### image_id, PredictionString
 ### 004f33259ee4aef671c2b95d54e4be68,14 1 0 0 1 1
 ### 004f33259ee4aef671c2b95d54e4be69,11 0.5 100 100 200 200 13 0.7 10 10 20 20
